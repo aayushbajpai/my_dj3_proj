9ff1e48b86476b0fac175cc2039f3c2a169dbfe4
aayush <aayushbajpai70@gmail.com>
2020-03-14 18:22:41 +0530

    uncomment fields in customer model

--- myapp/models.py
+++ myapp/models.py
@@ -63,9 +63,9 @@ class Tag(models.Model):
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    # phone = models.CharField(max_length=100)
-    # email = models.CharField(max_length=100)
-    # date_created = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
     salary = models.FloatField(null=True)
 
     
